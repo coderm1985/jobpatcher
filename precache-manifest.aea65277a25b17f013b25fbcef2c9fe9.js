0887e9e41d6f6c1115828e67b9c14a50d3587c42
coderm1985 <coderm1985@gmail.com>
2020-09-04 23:56:00 +0530

    react version of customer page

---
+++ precache-manifest.aea65277a25b17f013b25fbcef2c9fe9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b3d2a3861b7bd07a0bb0c1bb9a0b07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce37e5324c6dd934fb15",
+    "url": "/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "628551a9c15949d6a1fb",
+    "url": "/static/css/main.7bb8436e.chunk.css"
+  },
+  {
+    "revision": "ce37e5324c6dd934fb15",
+    "url": "/static/js/2.09864f29.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.09864f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "628551a9c15949d6a1fb",
+    "url": "/static/js/main.bcfda208.chunk.js"
+  },
+  {
+    "revision": "a8a7a2a2e5779aae9158",
+    "url": "/static/js/runtime-main.45bf1394.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
